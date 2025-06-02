ed6620e7ea79a77d8a6a1d11ec2bc151f67a9a5d
ben1980s <benbenson0718@gmail.com>
2025-06-02 14:24:51 +0800

    Site updated: 2025-06-02 14:24:51

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1748844634382";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1748845488940";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
